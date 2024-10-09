3fe41ebfd572c12474f923e7edbc377fcdefa437
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-10-09 00:40:56 -0500

    iconos side

--- bifront/src/components/sideDashboard/sideDashboard.js
+++ bifront/src/components/sideDashboard/sideDashboard.js
@@ -2,24 +2,24 @@ import { Nav, Navbar } from "react-bootstrap";
 // import {FaCalendarAlt} from "react-icons/fa";
 // import { MdDashboard, MdSchedule, MdHelp } from "react-icons/md";
 import "./sideDashboard.css";
-// import logo from "./biteLogo.png";
+import e1 from "./carbon_machine-learning-model.svg";
+import e2 from "./material-symbols_model-training-outline-rounded.svg";
 
 function SideDashboard() {
 	return (
 		<div className="sidebar-dashboard">
 			<Navbar className="flex-column vh-100">
 				<Navbar.Brand href="/" className="mx-auto my-3">
-					{/* <img src="" alt="Bite Back" className="logo" width="100" /> */}
 					<h1>UNFPA</h1>
 				</Navbar.Brand>
 				<Nav className="flex-column w-100 items">
 					<Nav.Link href="/" className="sidebar-item">
-						{/* <MdDashboard className="sidebar-icon" /> */}
-						Endpoint 1
+						<img src={e1} alt="logo" className="logo" />
+						<label>Endpoint 1</label>
 					</Nav.Link>
 					<Nav.Link href="/reentrenar" className="sidebar-item">
-						{/* <MdDashboard className="sidebar-icon" /> */}
-						Endpoint 2
+						<img src={e2} alt="logo" className="logo" />
+						<label>Endpoint 1</label>
 					</Nav.Link>
 				</Nav>
 			</Navbar>
