786796b289f4ac711399ea524f038773f9cf5d23
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-10-08 23:10:29 -0500

    cambios css

--- bifront/src/components/endpoint1/uploadFile/uploadFile.js
+++ bifront/src/components/endpoint1/uploadFile/uploadFile.js
@@ -3,25 +3,30 @@ import "./uploadFile.css";
 
 function UploadFile() {
 	return (
-		<>
-			<Card className="file-card">
-				<Card.Body>
-					<Card.Title>Sube tu archivo!</Card.Title>
-					<Card.Text>
+		<Card className="file-card">
+			<Card.Body>
+				<Card.Title>
+					<h2 className="upload-title">Sube tu archivo!</h2>
+				</Card.Title>
+				<Card.Text>
+					<p className="upload-text">
 						El archivo debe ser un <span className="csv-highlight">CSV</span>{" "}
-						que contenga los textos que requieras clasificar.
-						<Form>
-							<Form.Group controlId="formFile">
-								<Form.Control type="file" />
-							</Form.Group>
-							<Button variant="primary" type="submit" className="mt-3">
-								Subir archivo
-							</Button>
-						</Form>
-					</Card.Text>
-				</Card.Body>
-			</Card>
-		</>
+						que contenga los{" "}
+						<span className="csv-highlight">
+							textos que requieras clasificar.
+						</span>
+					</p>
+					<Form>
+						<Form.Group controlId="formFile">
+							<Form.Control type="file" />
+						</Form.Group>
+						<Button variant="primary" type="submit" className="mt-3">
+							Subir archivo
+						</Button>
+					</Form>
+				</Card.Text>
+			</Card.Body>
+		</Card>
 	);
 }
 export default UploadFile;
