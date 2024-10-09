8ce014f0f6fb6ec39b8555ac6a201341b9050efa
DannyC666 <d.munozs@uniandes.edu.co>
2024-10-08 22:14:03 -0500

    minor

---
+++ bifront/src/components/cardComponent/card.js
@@ -0,0 +1,12 @@
+// import { Nav, Navbar } from "react-bootstrap";
+// // import {FaCalendarAlt} from "react-icons/fa";
+// // import { MdDashboard, MdSchedule, MdHelp } from "react-icons/md";
+
+// // import logo from "./biteLogo.png";
+
+// function cardComp() {
+// 	return (
+// 		<h1> Carta de mierda </h1>
+// 	);
+// }
+// export default cardComp;
--- bifront/src/components/endpoint1/endpoint1.js
+++ bifront/src/components/endpoint1/endpoint1.js
@@ -1,10 +1,13 @@
 import SidebarDashboard from "../sideDashboard/sideDashboard";
-
+// import cardComp  from "../cardComponent/card";
 function Endpoint1() {
 	return (
 		<>
 			<SidebarDashboard />
+
 		</>
+
+	
 	);
 }
 export default Endpoint1;
